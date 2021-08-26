85bfc7342b220facd44f257d05275917a284974b
AjiteshMahalingam <69707961+AjiteshMahalingam@users.noreply.github.com>
2021-08-26 22:20:05 +0530

    Error correction

--- utils/geoCode.js
+++ utils/geoCode.js
@@ -6,7 +6,7 @@ const request = require('request');
 const geoCode = (address, callback) => {
     const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoidG9ydnVzIiwiYSI6ImNrcXdiMmg0czAwMmkydnB0ZGxuY3N2anEifQ.NrXOVD6SMc23x_5Fabun6g';
     request({url, json:true}, (err, {body}) => {
-        if(0){
+        if(err){
             callback('Unable to connect to location services :(', undefined);
         } else if (body.features.length === 0) {
             callback('Unable to find location. Try another match', undefined);
